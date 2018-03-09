fb35ebd7d2539f1bdb4e5a1aa94f37fdd0c2bc7a
Shuai Lang <aspiringls942@gmail.com>
2018-03-09 13:34:47 -0800

    added config file and github remote

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express'); 
 // common js module, requiring or sharing
 // like import, but it's in ES 2015, not in node yet.
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// app listens and handles requests
 const app = express();
-// listen and handle requests
+
+// client iD 214020500968-15h6qb5sb6725s8kdqsf3eqcbal8mpt5.apps.googleusercontent.com
+// client secret ZmUUn2QjEiLIYEdR7xhCQEda
+passport.use(new GoogleStrategy());
 
 //a route handler
-app.get('/', (req, res) => {
-	res.send({bye: 'dude'});
-});
+
+
+//passport part
 
 //Dynamic port binding.
 //Look in the underlying environment and find the port to listen to.
