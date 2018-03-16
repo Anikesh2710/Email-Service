8dd961e00d645af1bd38803662b17c8565f76866
Shuai Lang <aspiringls942@gmail.com>
2018-03-16 00:22:42 -0700

    add google strategy proxy setting

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
 		clientID: keys.googleClientID,
 		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
+		callbackURL: '/auth/google/callback',
+		proxy: true // otherwise the relative path above for redirect would be consider unsafe from google
 	}, (accessToken, refreshToken, profile, done) => {
 		// check if profile id already exists; yes ? skip : create a user.
 		// This a Asynchronous request when reach to mongo.
