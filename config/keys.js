93dafe99fe6903075143f011a0758c41ced1db64
Shuai Lang <aspiringls942@gmail.com>
2018-03-15 23:59:32 -0700

    fix bug and finish auth

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js - figure out what set of credentials to return
 // use enviroment variable from heroku
 
-{
+if(process.env.NODE_ENV === 'production'){
 	// in production env - return the prod set of keys
 	module.exports = require('./prod'); 
 } else {
